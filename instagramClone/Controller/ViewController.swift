1a94af8c58b304b7cc9241bd0e995039edc90997
mustii <mustii@mmk.one>
2018-07-28 15:09:58 +0300

    APP is working

--- instagramClone/Controller/ViewController.swift
+++ instagramClone/Controller/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		view.backgroundColor = .yellow
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
